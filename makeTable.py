7504d883fa4c9a0879f6e38aa34a563ad638bd88
benjyz <ben@cyphere.io>
2014-01-29 08:24:44 +0100

    as csv file

---
+++ makeTable.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+"""
+make markdown table from csv data
+"""
+
+def get_data():
+    """get as array"""
+    with open('alts.csv','r') as f:
+        lines = f.readlines()
+        lines = [line.replace('\n','') for line in lines]
+        arr = [line.split(';') for line in lines]
+        return arr
+
+def tier_write(f,data):
+    #todo: pull from csv head
+    headers = '| CCC | currency name  | launched | exchange listed |'
+    sep = '| ------------- |:-------------:| -----:|:-------------:|'
+
+    f.write(headers + '\n')
+    f.write(sep + '\n')
+
+    i = 1
+    for row in data:
+        line = '|'.join(row)
+        f.write('|' + line + '|\n')
+
+if __name__=='__main__':
+    with open('alts.md','w') as f:
+        f.write('# Alternative Cryptocurrencies\n')
+        coins = get_data()
+        tiers = set([x[-1] for x in coins])
+        tiers = sorted(tiers)
+        for tier in tiers:
+            print tier
+            f.write('\n## ' + tier + '\n\n')
+            tierdata = [x for x in coins if x[-1] == tier]
+            tier_write(f,tierdata)
