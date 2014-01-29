5b3da20b4fce17e58db9d4c88ba7f454810d9a93
benjyz <ben@cyphere.io>
2014-01-29 08:46:56 +0100

    beautify link

--- makeTable.py
+++ makeTable.py
@@ -11,12 +11,21 @@ def get_data():
         arr = [line.split(';') for line in lines]
         return arr
 
+def transform_data(data):
+    """markdown beautify"""
+    for d in data:
+        url = d[2]
+        if 'bitcointalk' in url:
+            url = '[bitcointalk](' + url + ')'
+        d[2] = url
+    return data
+
 def tier_write(f,data):
     #todo: pull from csv head
     head  = ['CCC','name','URL','launched','exchange listed','tier']
     
     headers = '|'.join(head)
-    sep = '| ------------- |:-------------:|:-----:|:-----:|:-------------:|'
+    sep = ':---:|:---:|:---:|:---:|:---:|:---:'
 
     f.write('|' + headers + '|\n')
     f.write(sep + '\n')
@@ -24,12 +33,13 @@ def tier_write(f,data):
     i = 1
     for row in data:
         line = '|'.join(row)
-        f.write('|' + line + '|\n')
+        f.write(line + '\n')
 
 if __name__=='__main__':
     with open('alts.md','w') as f:
         f.write('# Alternative Cryptocurrencies\n')
         coins = get_data()
+        coins = transform_data(coins)
         tiers = set([x[-1] for x in coins])
         tiers = sorted(tiers)
         for tier in tiers:
